1def46242006b40199a810cbbacec660793b337a
Dhruv Desai <dhruvdesai@Dhruvs-MacBook-Air-2.local>
2025-08-05 15:25:34 -0400

    Fixing endpoint

--- src/ToDoComponents/TaskUtil.ts
+++ src/ToDoComponents/TaskUtil.ts
@@ -73,4 +73,4 @@ export function renderTaskHierarchy(
 }
 
 export const useMock = false;
-export const endpoint = import.meta.env.VITE_APP_BACKEND_URL + "vaccation/";
+export const endpoint = import.meta.env.VITE_APP_BACKEND_URL + "vaccation";
